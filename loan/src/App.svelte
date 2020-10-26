2944005fed521ec1b5f2ff468b35dfc2d51ef305
Saikat Das <saikatdas0790@gmail.com>
2020-10-26 19:13:04 +0530

    remove JS travel-packing

--- loan/src/App.svelte
+++ loan/src/App.svelte
@@ -1,19 +1,32 @@
 <script lang="ts">
-  let interestRate = 3;
-  let loanAmount = 2_00_000;
-  let years = 30;
   const MONTHS_PER_YEAR = 12;
+  let interestRate: number;
+  let loanAmount: number;
+  let years: number;
 
-  $: months = years * MONTHS_PER_YEAR;
-  $: monthlyInterestRate = interestRate / 100 / MONTHS_PER_YEAR;
-  $: numerator = loanAmount * monthlyInterestRate;
-  $: denominator = 1 - (1 + monthlyInterestRate) ** -months;
-  $: payment =
-    !loanAmount || !years
-      ? 0
-      : interestRate
-      ? numerator / denominator
-      : loanAmount / months;
+  const calculatePayment = (
+    loanAmount: number,
+    interestRate: number,
+    years: number,
+  ): number => {
+    if (!loanAmount || !years) return 0;
+    const months = years * MONTHS_PER_YEAR;
+    if (!interestRate) return loanAmount / months;
+    const monthlyInterestRate = interestRate / 100 / MONTHS_PER_YEAR;
+    const numerator = loanAmount * monthlyInterestRate;
+    const denominator = 1 - (1 + monthlyInterestRate) ** -months;
+    return numerator / denominator;
+  };
+
+  const reset = (): void => {
+    interestRate = 3;
+    loanAmount = 2_00_000;
+    years = 30;
+  };
+
+  reset();
+
+  $: payment = calculatePayment(loanAmount, interestRate, years);
 </script>
 
 <label for="loan">Loan Amount</label>
@@ -26,3 +39,5 @@
 <input type="number" id="years" bind:value={years} />
 
 <div>Monthly payment: ${payment.toFixed(2)}</div>
+
+<button on:click={reset}>Reset</button>
